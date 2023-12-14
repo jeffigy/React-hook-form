6fd3805a6065cb3488dddb402a9ca412ea7de349
jeffigy <ejeffydev@gmail.com>
2023-12-15 00:04:01 +0800

    devtools visualization

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -10,36 +10,40 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 type YouTubeFormProps = {};
 
 const YouTubeForm: React.FC<YouTubeFormProps> = () => {
   const form = useForm();
-  const { register } = form;
+  const { register, control } = form;
 
   return (
-    <form action="">
-      <Card>
-        <CardBody as={Stack} spacing={"10px"}>
-          <FormControl>
-            <FormLabel>Username</FormLabel>
-            <Input id="username" type="text" {...register("username")} />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input id="email" type="email" {...register("email")} />
-          </FormControl>
-          <FormControl id="channel">
-            <FormLabel>Channel</FormLabel>
-            <Input id="channel" type="text" {...register("channel")} />
-          </FormControl>
-        </CardBody>
-        <CardFooter>
-          <Button w={"full"} type="submit" colorScheme="blue">
-            Submit
-          </Button>
-        </CardFooter>
-      </Card>
-    </form>
+    <>
+      <form action="">
+        <Card>
+          <CardBody as={Stack} spacing={"10px"}>
+            <FormControl>
+              <FormLabel>Username</FormLabel>
+              <Input id="username" type="text" {...register("username")} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input id="email" type="email" {...register("email")} />
+            </FormControl>
+            <FormControl id="channel">
+              <FormLabel>Channel</FormLabel>
+              <Input id="channel" type="text" {...register("channel")} />
+            </FormControl>
+          </CardBody>
+          <CardFooter>
+            <Button w={"full"} type="submit" colorScheme="blue">
+              Submit
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 export default YouTubeForm;
